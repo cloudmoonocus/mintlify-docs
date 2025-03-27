46b649e765e3180a8da036c364b783406868fe49
fengshaokang <fengshaokang@meituan.com>
2025-03-27 11:04:00 +0800

    Hello World

---
+++ Java/learn-Java/src/Hello.java
@@ -0,0 +1,9 @@
+// 变量之间的赋值
+public class Hello {
+    public static void main(String[] args) {
+        String[] names = {"ABC", "XYZ", "zoo"};
+        String s = names[1];
+        names[1] = "cat";
+        System.out.println(s); // s是"XYZ"还是"cat"?
+    }
+}
